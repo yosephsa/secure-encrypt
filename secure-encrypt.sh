d652f650a69d6059509d650e7c4ccb8465dff6d5
Yoseph Alabdulwahab <10735559+yoseph1998@users.noreply.github.com>
2021-01-21 18:52:46 -0600

    Added SSD warning & bug fix

--- secure-encrypt.sh
+++ secure-encrypt.sh
@@ -10,7 +10,7 @@
 # 
 # Notes on usage:
 #	if filename does not end with .gpg extension then it will attempt to encrypt it. 
-#	However if it does end with .gpg extension then it will attempt to decrypt it
+#	However if it does end with .gpg extension then it will attempt to decrypt it 
 
 does_package_exist () { 
 	package=$1
@@ -36,23 +36,24 @@ does_package_exist () {
 #Check if gpg installed correctly and is usable.
 #If so select the desired file and encrypt
 if does_package_exist gpg; then
-	if ! [ -z $1 ] && [ -e $1 ]; then #input is given, set to filename
+	if ! [ -z "$1" ] && [ -e "$1" ]; then #input is given, set to filename
 		filename=$1
 	else #Ask for a filename
 		read -e -p "Enter filepath / filename: " filename
 	fi
-	if ! [ -e $filename ]; then
+	if ! [ -e "$filename" ]; then
 		echo "The file/filepath you specified does not exist. Please double check that and try again."
+		exit 1
 	else
 		#determine if we are encrypting or decrypting
 		if [[ ${filename: -4} == ".gpg" ]]; then #decrypting
-			echo "gpg extension detected. Attempting to decrypt it..."
-			gpg -o ${filename::-4} -d $filename
+			echo ".gpg extension detected. Attempting to decrypt it..."
+			gpg -o "${filename::-4}" -d "$filename"
 			exit 0 #stop executing script to prevent secure deletion of encrypted input
 		else #encrypting
 			does_package_exist wipe #Prompt to install (if needed) wipe before we start encrypting
 			echo "Encrypting source file ($filename)..."
-			gpg -c $filename
+			gpg -c "$filename"
 			if ! [ -e "${filename}.gpg" ]; then
 				echo "Could not locate encrypted file. Skipping secure wipe of source file"
 				exit 1
@@ -64,8 +65,14 @@ fi
 #Check if gpg installed correctly and is usable.
 #If so select the desired file and encrypt
 if does_package_exist wipe; then
+	#Warn user if using an SSD
+	[[ $(df "$filename") =~ sd[a-z] ]]; #Look for sdX in output of df .
+	isRota=$(cat /sys/block/${BASH_REMATCH[0]}/queue/rotational) #Run rotery check. 1 is hdd 0 is ssd
+	if [ "$isRota" == 0 ]; then
+		echo "WARNING: A secure delete is not possible on a SSD. For more information - https://github.com/yoseph1998/secure-encrypt"
+	fi
 	echo "Securely deleting source file..."
-	wipe $filename
+	wipe "$filename"
 else
 	echo "Could not securely wipe source file."
 	exit 1
