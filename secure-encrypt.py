747eb7bcba36d13531b35317d5ef9d9cac2f63ff
Yoseph Alabdulwahab <10735559+yoseph1998@users.noreply.github.com>
2021-01-24 14:31:58 -0600

    Improved cli and bug fixes

--- secure-encrypt.py
+++ secure-encrypt.py
@@ -51,7 +51,7 @@ def encrypt_file(filepath, passwd):
 				recipients=None)
 	except FileNotFoundError:
 		print(f"ERROR: Could not open file '{filepath}'. Make sure you entered the correct file")
-		return False #Abort function
+		quit(1)
 
 # A method to decrypt a file using the provided password.
 #
@@ -60,11 +60,6 @@ def encrypt_file(filepath, passwd):
 #
 # Output: Will create a file named [filepath]<minus the .gpg extension> containing the decrypted content
 def decrypt_file(filepath, passwd):
-	#double check we are decrypting a .gpg file if so prep
-	if(not filepath.endswith(".gpg")):
-		print(f"ERROR: File '{filepath}' is not a .gpg file.")
-		return False
-	
 	#init GnuPG
 	gpg = gnupg.GPG()
 
@@ -76,7 +71,7 @@ def decrypt_file(filepath, passwd):
 			status = gpg.decrypt_file(file, passphrase=passwd, output=filepath_o)
 	except FileNotFoundError:
 		print(f"ERROR: Could not open file '{filepath}'. Make sure you entered the correct filepath")
-		return False #Abort function
+		quit(1)
 
 ###################################################################################################
 ## I/O Methods ####################################################################################
@@ -90,13 +85,15 @@ def decrypt_file(filepath, passwd):
 # 		Return false if filepath is not a string
 #		Return true if filepath is valid
 #		Print error and Quit program if filepath does not exists
-def validate_filepath(filepath):
-	filepath = sys.argv[0]
+def validate_filepath(filepath, quit=False, debug=True):
 	if(not isinstance(filepath, str)):
 		return False
 	elif(not path.exists(filepath)):
-		print(f"ERROR! File '{filepath}' does not exist. Please double check and try again")
-		exit(1) #Quit program
+		if(debug):
+			print(f"ERROR! File '{filepath}' does not exist. Please double check and try again")
+		if(quit):
+			exit(1) #Quit program
+		return False
 	else:
 		return True
 
@@ -124,21 +121,34 @@ def get_password(asktwice = False):
 
 #Print script usage
 def print_usage():
-	print("\n Usage: ")
-	print("    secure-encrypt.py encrypt <filepath>			# Will encrypt said file and wipe it")
-	print("    secure-encrypt.py decrypt <filepath>			# Will decrypt said file")
-	print("    secure-encrypt.py wipe <filepath>			# Will only wipe said file")
+	print("\n   Usage:")
+	print(f"      python {sys.argv[0]} <command> <filepath>")
+	print("      or `alias` <command> <filepath>")
+	print("\n   Commands:")
+	print("      encrypt | -e    # Encrypt file and wipe")
+	print("      decrypt | -d    # Decrypt file")
+	print("      wipe    | -w       # Wipe\n")
+
 
 # Wipe file
 def wipe(filepath):
-	print(f"Secure wiping file {filepath}...")
+	#Validate filepath. Quit if does not exist
+	validate_filepath(filepath, quit=True)
+
+	#Give user warning on ssd and non standard filesystems
+	print(f"Secure wiping file {filepath} ...")
 	print("    WARNING: This only works on Windows/Linux on standard filesystems using a HDD. \
-				FILES MAY STILL BE RECOVERABLE ON SSDs.")
+FILES MAY STILL BE RECOVERABLE ON SSDs.")
+
+	#Wipe
 	secure_delete.secure_random_seed_init()
 	secure_delete.secure_delete(filepath)
 
 #Encrypt file. Will also wipe it
 def encrypt(filepath):
+	#Validate filepath. Quit if does not exist
+	validate_filepath(filepath, quit=True)
+
 	#Get password
 	passwd = get_password(asktwice = True)
 
@@ -156,6 +166,9 @@ def encrypt(filepath):
 
 #Decrypt file
 def decrypt(filepath):
+	#Validate filepath. Quit if does not exist
+	validate_filepath(filepath, quit=True)
+
 	#Get password
 	passwd = get_password()
 
@@ -167,45 +180,52 @@ def decrypt(filepath):
 ## START OF SCRIPT ################################################################################
 ###################################################################################################
 
-#Guess if we are encrypting or decrypting
-if(len(sys.argv) == 2):
-	print("WARNING: no command supplied. Trying to guess what you mean ...")
-	arg0 = sys.argv[1]
-
-	#Make sure file exists before proceding 
-	validate_filepath(arg0)
-
-	#Decide between encrypting or decrypting
-	if(arg0[-4:] == '.gpg'): #Decrypt file
-		decrypt(arg0)
-		exit(0) #Quit program
-	else: #Encrypt said file
-		encrypt(arg0)
-		exit(0)
-
-#Guess if we are encrypting or decrypting
-elif(len(sys.argv) == 3):
-	#Prepare arguments
-	arg0 = sys.argv[1]
-	arg1 = sys.argv[2]
-
-	#Determine the argument entered
-	if(arg0 == "encrypt"):
-		encrypt(arg1)
-	elif(arg0 == "decrypt"):
-		decrypt(arg1)
-	elif(arg0 == "wipe"):
-		wipe(arg1)
-	else: #Unknown know command
-		print(f"ERROR: Unknown command '{arg0}'. Please run the script as follows: ")
-		print_usage()
-
-#Invalid number of arguments. Throw error.
-else: 
+#Check if minimum arguments is provided
+if(len(sys.argv) < 2 or len(sys.argv) > 3):
 	print("ERROR: Invalid number of arguments. Please run the script as follows: ")
 	print_usage()
 	exit(1) #Quit program
 
+arg0 = sys.argv[1]
+
+#See if first arg is a command
+if(arg0 == "encrypt" or arg0 == "-e"):
+	if(len(sys.argv) != 3):
+		print("ERROR: Filepath to encrypt not specified.")
+		exit(1) #Quit program
+	#Encrypt and Wipe file
+	encrypt(sys.argv[2])
+elif(arg0 == "decrypt" or arg0 == "-d"):
+	if(len(sys.argv) != 3):
+		print("ERROR: Filepath to decrypt not specified.")
+		exit(1) #Quit program
+	#Decrypt file
+	decrypt(sys.argv[2])
+elif(arg0 == "wipe" or arg0 == "-w"):
+	if(len(sys.argv) != 3):
+		print("ERROR: Filepath to wipe not specified.")
+		exit(1) #Quit program
+	#Wipe file
+	wipe(sys.argv[2])
+
+#See if first argument is a file
+else: #Unknown know command
+	print("WARNING: no command supplied. Trying to guess what you mean ...")
+	arg0 = sys.argv[1]
+
+	if(validate_filepath(arg0, debug=False)):
+		#Decide between encrypting or decrypting
+		if(arg0[-4:] == '.gpg'): #Decrypt file
+			decrypt(arg0)
+		else: #Encrypt said file
+			encrypt(arg0)
+	else:
+		print("\nInvalid usage. Please run script as follows:")
+		print("(If you meant to type a filename, then it does not exist)")
+		print_usage()
+		exit(1)
 
 
-print("Complete.\n")
+	
+
+print("Complete.")
